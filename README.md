d8570ae6ace312511757bfea9f83a53fd0fc1178
SiapaLupa <muhammadilyasafadhlih@gmail.com>
2023-05-13 13:16:03 +0700

    fixed typo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Palindrome Checker in typescript
+
+This is a palindrome checker, palindrome mean you can read the text from both left side or right side
--- mod.ts
+++ mod.ts
@@ -1,3 +1,3 @@
 export default function checkPalindrome(text: string, ignoreCase: boolean = true): boolean {
-  return new RegExp(text, ignoreCase ? "g" : "gi").test(text.split("").reverse().join(""));
+  return new RegExp(text, ignoreCase ? "gi" : "g").test(text.split("").reverse().join(""));
 }
