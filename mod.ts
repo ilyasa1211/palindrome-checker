54b11031f0e0e6221e81cc28670bc21a848127a3
Muhammad Ilyasa Fadhlih <110075636+ilyasa1211@users.noreply.github.com>
2023-09-30 17:22:33 +0700

    Update mod.ts
    change 'check'to 'is'

--- mod.ts
+++ mod.ts
@@ -1,3 +1,3 @@
-export default function checkPalindrome(text: string, ignoreCase: boolean = true): boolean {
+export default function isPalindrome(text: string, ignoreCase: boolean = true): boolean {
   return new RegExp(text, ignoreCase ? "gi" : "g").test(text.split("").reverse().join(""));
 }
